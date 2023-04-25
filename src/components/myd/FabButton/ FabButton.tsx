5436bd7e65f0db94f8b54ca37e53d0397c60d3ae
Arferik <610231551@qq.com>
2023-04-25 22:51:23 +0800

    feat(fabbutton): 新增 fab

--- src/components/myd/FabButton/ FabButton.tsx
+++ src/components/myd/FabButton/ FabButton.tsx
@@ -8,18 +8,50 @@ export type FabButtonColor = "primary" | "surface" | "secondary" | "tertiary";
 export interface FabButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement> {
   color?: FabButtonColor;
-  size?: "sm" | "md" | "lg";
+  size?: "sm" | "md" | "lg" | number;
   icon: string;
+  elevation?: "default" | "lowered";
 }
 
-const IconRoot = styled(Icon)<FabButtonProps>(({ color = "primary" }) => [
+const IconRoot = styled(Icon)<FabButtonProps>(({ color = "primary", size }) => [
+  size === "lg" && tw`text-4xl`,
   color === "primary" && tw`text-on-primary-container `,
+  color === "surface" && tw`text-on-surface-variant`,
+  color === "secondary" && tw`text-on-secondary-container`,
+  color === "tertiary" && tw`text-on-tertiary-container`,
 ]);
 
-const ButtonRoot = styled(BaseButton)(({ color = "primary", size = "md" }) => [
-  size === "md" && tw`w-14 h-14 rounded-2xl`,
-  color === "primary" && tw`bg-primary-container shadow-md`,
-]);
+const ButtonRoot = styled(BaseButton)<FabButtonProps>(
+  ({ color = "primary", size = "md", elevation = "default" }) => [
+    elevation === "default"
+      ? tw`shadow-md hover:shadow-lg`
+      : tw`shadow hover:shadow-md`,
+    size === "md" && tw`w-14 h-14 rounded-2xl`,
+    size === "sm" && tw`w-10 h-10 rounded-xl`,
+    size === "lg" && tw`w-24 h-24 rounded-[1.75rem]`,
+    color === "primary" &&
+      tw`bg-primary-container 
+        hover:after:(w-[200%] h-[200%] bg-on-primary-container opacity-[.08] absolute top-[-50%] left-[-50%])
+        active:after:(w-[200%] h-[200%] bg-on-primary-container opacity-[.12] absolute top-[-50%] left-[-50%])
+    `,
+    color === "surface" &&
+      tw`bg-surface-variant 
+        hover:after:(w-[200%] h-[200%] bg-on-surface-variant opacity-[.08] absolute top-[-50%] left-[-50%])
+        active:after:(w-[200%] h-[200%] bg-on-surface-variant  opacity-[.12] absolute top-[-50%] left-[-50%])
+    `,
+    color === "secondary" &&
+      tw`bg-secondary-container 
+        hover:after:(w-[200%] h-[200%] bg-on-secondary-container opacity-[.08] absolute top-[-50%] left-[-50%])
+        active:after:(w-[200%] h-[200%] bg-on-secondary-container  opacity-[.12] absolute top-[-50%] left-[-50%])
+    
+    `,
+    color === "tertiary" &&
+      tw`bg-tertiary-container
+        hover:after:(w-[200%] h-[200%] bg-on-tertiary-container opacity-[.08] absolute top-[-50%] left-[-50%])
+        active:after:(w-[200%] h-[200%] bg-on-tertiary-container  opacity-[.12] absolute top-[-50%] left-[-50%])
+    `,
+  ]
+);
 
 const FabButton = React.forwardRef(function FabButton(
   props: React.ComponentProps<"button"> & FabButtonProps,
@@ -27,7 +59,11 @@ const FabButton = React.forwardRef(function FabButton(
 ) {
   return (
     <ButtonRoot {...props} className={clsx(props.className)} ref={ref}>
-      <IconRoot {...props} size="md" name={props.icon}></IconRoot>
+      <IconRoot
+        {...props}
+        size={props.size === "lg" ? 36 : "md"}
+        name={props.icon}
+      ></IconRoot>
     </ButtonRoot>
   );
 });
